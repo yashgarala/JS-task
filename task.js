a567b2ff890d2c0986bb5407307247ac281ddd81
yashgarala <yash.garala@bacancy.com>
2022-01-25 16:44:30 +0530

    task done

---
+++ task.js
@@ -0,0 +1,22 @@
+console.log("test");
+
+var  get_by_id = document.getElementById("id-list-of-item");
+
+console.log("get element by id " , get_by_id);
+
+var  get_by_class = document.getElementsByClassName("list-item");
+
+console.log("get_by_class",get_by_class);
+
+
+var  get_by_tag_name = document.getElementsByTagName("h1");
+
+console.log("get_by_tag_name",get_by_tag_name);
+
+var  get_by_qurey = document.querySelector(".list-item");
+
+console.log("get_by_qurey",get_by_qurey)
+
+var  get_by_qureyall = document.querySelectorAll(".list-item");
+
+console.log("get_by_qureyall",get_by_qureyall)
